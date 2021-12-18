f7ed20b33d2b7c74657f3119ae60fbdf6c1d14c0
dm1tro69 <dimolg22@gmail.com>
2021-12-18 20:59:23 +0200

    add reset password

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import {getAuth, sendPasswordResetEmail} from 'firebase/auth'
+import {ReactComponent as ArrowRightIcon} from "../assets/svg/keyboardArrowRightIcon.svg";
+import {Link, useNavigate} from "react-router-dom";
 
 const ForgotPassword = () => {
+    const [email, setEmail] = useState('')
+
+    const navigate = useNavigate()
+
+    const onChange = (e:ChangeEvent<HTMLInputElement>) => {
+         setEmail(e.target.value)
+    }
+    const onSubmit = async (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            await sendPasswordResetEmail(auth, email)
+            navigate('/')
+
+        }catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <div>
-            <h1>Forgot Password</h1>
+        <div className={'pageContainer'}>
+            <header>
+                <p className={'pageHeader'}>Forgot Password</p>
+            </header>
+            <main>
+                <form onSubmit={onSubmit}>
+                    <input
+                        placeholder={'Email'}
+                        id={'email'}
+                        value={email}
+                        onChange={onChange}
+                        className={'emailInput'}
+                        type="email"
+                    />
+                    <Link className={'forgotPasswordLink'} to={'/sign-in'}>Sign In</Link>
+                    <div className={'signInBar'}>
+                       <div className={'signInText'}>
+                           Send Reset Link
+                       </div>
+                        <button className={'signInButton'}>
+                            <ArrowRightIcon fill={'#fff'} width={'34px'} height={'34px'}/>
+                        </button>
+                    </div>
+                </form>
+            </main>
+
         </div>
     );
 };
